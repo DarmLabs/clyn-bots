a0b9636943e4585a7e1d30ae631ddeb55b8367ab
BDeWinne <68663886+BDeWinne@users.noreply.github.com>
2022-06-22 18:55:41 -0300

    Exit minigames

---
+++ Assets/Scripts/UI/Minigames/MG_UI_Manager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MG_UI_Manager : MonoBehaviour
+{
+    public void BackTo3D()
+    {
+        SceneManager.LoadScene("Main_Stage");
+    }
+}
