cf8c61bf0a7e771acbfc77a08fa66958c851b335
BDeWinne <68663886+BDeWinne@users.noreply.github.com>
2022-06-22 18:55:55 -0300

    Camera fixes

--- Assets/Scripts/Camera/SmoothFollow.cs
+++ Assets/Scripts/Camera/SmoothFollow.cs
@@ -8,9 +8,14 @@ public class SmoothFollow : MonoBehaviour
 
     float smoothSpeed = 10f;
     public Vector3 offset;
+    Vector3 targetOffset;
+    void Start()
+    {
+        targetOffset = new Vector3(0, 1.3f,0);
+    }
     void LateUpdate()
     {
-        Vector3 desiredPosition = target.position + offset;
+        Vector3 desiredPosition = (target.position + targetOffset) + offset;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
         transform.position = smoothedPosition;
     }
