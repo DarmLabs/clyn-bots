28f3013bbf70b0515dc645dda66344cb966a84ff
BDeWinne <68663886+BDeWinne@users.noreply.github.com>
2022-06-29 15:11:43 -0300

    URP + construccion casi hecha

---
+++ Assets/Scripts/Animations/MolinoAnim.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MolinoAnim : MonoBehaviour
+{
+    void LateUpdate()
+    {
+        transform.Rotate(-Vector3.right, Space.World);
+    }
+}
