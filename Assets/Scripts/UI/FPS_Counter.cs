026ead786e3a8afdcb7f7e1ae360f305b9c6fa0c
BDeWinne <68663886+BDeWinne@users.noreply.github.com>
2022-06-30 21:45:53 -0300

    fpsCounter

---
+++ Assets/Scripts/UI/FPS_Counter.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class FPS_Counter : MonoBehaviour
+{
+    public TextMeshProUGUI fpsText;
+    private float pollingTime = 1f;
+    private float time;
+    private int frameCount;
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        frameCount++;
+        if(time >= pollingTime){
+            int frameRate = Mathf.RoundToInt(frameCount/time);
+            fpsText.text = frameRate.ToString() + " FPS";
+
+            time -= pollingTime;
+            frameCount = 0;
+        }
+    }
+}
