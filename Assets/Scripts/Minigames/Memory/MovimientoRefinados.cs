11cf11289e026b81c738339b6883563dac327ca0
julagui <julioaguilar-ap@outlook.com>
2022-07-08 21:58:47 -0300

    Movimiento mejorado, agregada espera

--- Assets/Scripts/Minigames/Memory/MovimientoRefinados.cs
+++ Assets/Scripts/Minigames/Memory/MovimientoRefinados.cs
@@ -9,15 +9,27 @@ public class MovimientoRefinados : MonoBehaviour
     [SerializeField] private GameObject cartonRefinadoUbicacion;
     [SerializeField] private GameObject plasticoRefinadoUbicacion;
     [SerializeField] private GameObject compostRefinadoUbicacion;
-    private float fraction = 0.4f;
+    private float fraction = 0.2f;
+    private float currentTime = 0f;
+    private float startingTime = 3f;
 
-    
+
+    void Start() 
+    {
+        currentTime = startingTime;
+    }
     void Update()
     {
-        this.gameObject.transform.position = Vector3.Slerp(this.gameObject.transform.position, vidrioRefinadoUbicacion.transform.position, fraction*Time.deltaTime); 
-        this.gameObject.transform.position = Vector3.Slerp(this.gameObject.transform.position, metalRefinadoUbicacion.transform.position, fraction*Time.deltaTime);
-        this.gameObject.transform.position = Vector3.Slerp(this.gameObject.transform.position, cartonRefinadoUbicacion.transform.position, fraction*Time.deltaTime);
-        this.gameObject.transform.position = Vector3.Slerp(this.gameObject.transform.position, plasticoRefinadoUbicacion.transform.position, fraction*Time.deltaTime);
-        this.gameObject.transform.position = Vector3.Slerp(this.gameObject.transform.position, compostRefinadoUbicacion.transform.position, fraction*Time.deltaTime);
+        currentTime -= 1*Time.deltaTime;
+        if (currentTime <= 0)
+        {
+            this.gameObject.transform.position = Vector3.Slerp(this.gameObject.transform.position, vidrioRefinadoUbicacion.transform.position, fraction*Time.deltaTime); 
+            this.gameObject.transform.position = Vector3.Slerp(this.gameObject.transform.position, metalRefinadoUbicacion.transform.position, fraction*Time.deltaTime);
+            this.gameObject.transform.position = Vector3.Slerp(this.gameObject.transform.position, cartonRefinadoUbicacion.transform.position, fraction*Time.deltaTime);
+            this.gameObject.transform.position = Vector3.Slerp(this.gameObject.transform.position, plasticoRefinadoUbicacion.transform.position, fraction*Time.deltaTime);
+            this.gameObject.transform.position = Vector3.Slerp(this.gameObject.transform.position, compostRefinadoUbicacion.transform.position, fraction*Time.deltaTime);
+        }
+
+        
     }
 }
