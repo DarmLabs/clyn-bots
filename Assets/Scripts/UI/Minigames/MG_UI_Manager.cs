f6249070c855cde451644814cf06184814cd63e6
julagui <julioaguilar-ap@outlook.com>
2022-08-01 20:15:51 -0300

    Corrección para Reintentar()

--- Assets/Scripts/UI/Minigames/MG_UI_Manager.cs
+++ Assets/Scripts/UI/Minigames/MG_UI_Manager.cs
@@ -30,9 +30,39 @@ public class MG_UI_Manager : MonoBehaviour
 
     public void Reintentar()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        Grilla.vidas = 20;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);        
         Time.timeScale = 1f;
+        switch (Tachos.CantidadGrilla)
+        {
+            case 24:            
+            Grilla.vidas = 60; //30
+            Cards.CantidadPares = 12;
+            break;
+            case 22:
+            Grilla.vidas = 50; //25 
+            Cards.CantidadPares = 11;
+            break;
+            case 20:
+            Grilla.vidas = 40; //20  
+            Cards.CantidadPares = 10;  
+            break;
+            case 18:
+            Grilla.vidas = 30;  //15    
+            Cards.CantidadPares = 9; 
+            break;
+            case 16:
+            Grilla.vidas = 24; //12  
+            Cards.CantidadPares = 8;   
+            break;
+            case 14:
+            Grilla.vidas = 22; //11   
+            Cards.CantidadPares = 7;  
+            break;
+            case 12:
+            Grilla.vidas = 20; //10  
+            Cards.CantidadPares = 6;    
+            break;        
+        }        
     }
 
     public void RetomarTiempo()
