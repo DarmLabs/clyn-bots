22bd1190c6181049b8e57cd9614681b75e47c8d8
julagui <julioaguilar-ap@outlook.com>
2022-08-09 15:10:02 -0300

    Escalas

--- Assets/Scenes/Mingames/Pipes/RotatePipe.cs
+++ Assets/Scenes/Mingames/Pipes/RotatePipe.cs
@@ -9,6 +9,14 @@ public class RotatePipe : MonoBehaviour
         if(!PipeController.gano)
         {
             transform.Rotate(0f,0f,90f);
+            if (transform.rotation.z == 0)
+            {
+                transform.localScale = new Vector3(1.09f,1.09f,1.09f);
+            }
+            else
+            {
+                transform.localScale = new Vector3(0.91f,0.91f,0.91f);
+            }
         }
    }
 }
