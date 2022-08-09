dc849c5fe565d9f0a9a0670ff621be0b0a6df0bb
julagui <julioaguilar-ap@outlook.com>
2022-08-09 16:06:38 -0300

    Correcciones y errores nuevos PipesPuzzle

--- Assets/Scenes/Mingames/Pipes/PipeController.cs
+++ Assets/Scenes/Mingames/Pipes/PipeController.cs
@@ -16,13 +16,14 @@ public class PipeController : MonoBehaviour
     void Update()
     {
         if (Tubos[0].rotation.z == 0 && Tubos[1].rotation.z == 0 && Tubos[2].rotation.z == 0 && Tubos[3].rotation.z == 0 && Tubos[4].rotation.z == 0 &&
-        Tubos[5].rotation.z == 0 && Tubos[9].rotation.z == 0 && Tubos[13].rotation.z == 0 && Tubos[17].rotation.z == 0 && Tubos[21].rotation.z == 0 &&
-        Tubos[6].rotation.z == 0 && Tubos[10].rotation.z == 0 && Tubos[14].rotation.z == 0 && Tubos[18].rotation.z == 0 && Tubos[22].rotation.z == 0 &&
-        Tubos[7].rotation.z == 0 && Tubos[11].rotation.z == 0 && Tubos[15].rotation.z == 0 && Tubos[19].rotation.z == 0 && Tubos[23].rotation.z == 0 &&
-        Tubos[8].rotation.z == 0 && Tubos[12].rotation.z == 0 && Tubos[16].rotation.z == 0 && Tubos[20].rotation.z == 0 && Tubos[24].rotation.z == 0)
+            Tubos[5].rotation.z == 0 && Tubos[9].rotation.z == 0 && Tubos[13].rotation.z == 0 && Tubos[17].rotation.z == 0 && Tubos[21].rotation.z == 0 &&
+            Tubos[6].rotation.z == 0 && Tubos[10].rotation.z == 0 && Tubos[14].rotation.z == 0 && Tubos[18].rotation.z == 0 && Tubos[22].rotation.z == 0 &&
+            Tubos[7].rotation.z == 0 && Tubos[11].rotation.z == 0 && Tubos[15].rotation.z == 0 && Tubos[19].rotation.z == 0 && Tubos[23].rotation.z == 0 &&
+            Tubos[8].rotation.z == 0 && Tubos[12].rotation.z == 0 && Tubos[16].rotation.z == 0 && Tubos[20].rotation.z == 0 && Tubos[24].rotation.z == 0)
         {
             gano = true;
             Debug.Log("GANASTE NIÑO BOBO");            
         }
+        
     }
 }
--- Assets/Scenes/Mingames/Pipes/RotatePipe.cs
+++ Assets/Scenes/Mingames/Pipes/RotatePipe.cs
@@ -4,19 +4,38 @@ using UnityEngine;
 
 public class RotatePipe : MonoBehaviour
 {
+   [SerializeField] private Vector3[] ArrayRotaciones;   
+   private int randomIndex = 0;
+   
+
+   void Start()
+   {
+        randomIndex = Random.Range(0,3);
+        transform.Rotate(ArrayRotaciones[randomIndex]);
+        Debug.Log("pieza: "+this.gameObject.name+"  array:"+ArrayRotaciones[randomIndex]);
+   }
+
+   void Update() 
+   {
+        if (transform.rotation == Quaternion.identity)
+        {
+            transform.localScale = new Vector3(1.09f,1.09f,1.09f);
+        }            
+        else
+        {
+            transform.localScale = new Vector3(0.91f,0.91f,0.91f);
+        }
+   }
+
    void OnMouseDown() 
    {
         if(!PipeController.gano)
         {
-            transform.Rotate(0f,0f,90f);
-            if (transform.rotation.z == 0)
+            if(transform.rotation.z != 0)
             {
-                transform.localScale = new Vector3(1.09f,1.09f,1.09f);
+                transform.Rotate(0f,0f,90f);
             }
-            else
-            {
-                transform.localScale = new Vector3(0.91f,0.91f,0.91f);
-            }
+                        
         }
    }
 }
