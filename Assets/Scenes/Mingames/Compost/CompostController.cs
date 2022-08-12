6b3c50b367c96dd9c5c95896002f3016253220a4
julagui <julioaguilar-ap@outlook.com>
2022-08-12 03:11:07 -0300

    Paneles de victoria/derrota - Cambio de colores según valores

--- Assets/Scenes/Mingames/Compost/CompostController.cs
+++ Assets/Scenes/Mingames/Compost/CompostController.cs
@@ -1,27 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CompostController : MonoBehaviour
 {
+    [SerializeField] private GameObject PanelVictoria;
+    [SerializeField] private GameObject PanelDerrota;
+    [SerializeField] private GameObject UI_Desactivar;
+    [SerializeField] Transform BotonSecar;
+    [SerializeField] Transform BotonHumedecer;
+    [SerializeField] Transform BotonRemover;
+    
     [SerializeField] Transform ProgresoHumedo;
     [SerializeField] Transform ProgresoOlor;
     [SerializeField] Transform ProgresoSeco;
     private float EscalaMax = 1f;
     private float EscalaMin = 0f;
-    [SerializeField] Transform BotonSecar;
-    [SerializeField] Transform BotonHumedecer;
-    [SerializeField] Transform BotonRemover;
+    
     private Vector3 escalaProgreso_Humedo;
     private Vector3 escalaProgreso_Olor;
     private Vector3 escalaProgreso_Seco;
+
     //0 Humeda - 1 Seca - 2 Olor - 3 Correcta
-    [SerializeField] Transform Compostera;
-    private int randomIndex;  
+    [SerializeField] Transform Compostera;    
+    private int randomIndex;
     private bool gano = false;
 
+    [SerializeField] private Image BarraHumedo;
+    [SerializeField] private Image BarraOlor;
+    [SerializeField] private Image BarraSeco;    
+
     void Start()
-    {
+    {        
         gano = false;
         randomIndex = Random.Range(0,3);
         Compostera.GetChild(randomIndex).gameObject.SetActive(true);        
@@ -60,6 +71,8 @@ public class CompostController : MonoBehaviour
     void Update()
     {
         VictoriaDerrota();
+        CambiarCompostActiva();
+        ActualizarColores();
     }
 
     public void Boton_Secar()
@@ -101,26 +114,56 @@ public class CompostController : MonoBehaviour
         if (escalaProgreso_Seco.x >= EscalaMax)
         {
             Debug.Log("PERDISTE NIÑITO :(");
+            PanelDerrota.SetActive(true);
+            UI_Desactivar.SetActive(false);
+            BotonHumedecer.gameObject.SetActive(false);
+            BotonSecar.gameObject.SetActive(false);
+            BotonRemover.gameObject.SetActive(false);
         }
         if (escalaProgreso_Seco.x <= EscalaMin)
         {
             Debug.Log("PERDISTE NIÑITO :(");
+            PanelDerrota.SetActive(true);
+            UI_Desactivar.SetActive(false);
+            BotonHumedecer.gameObject.SetActive(false);
+            BotonSecar.gameObject.SetActive(false);
+            BotonRemover.gameObject.SetActive(false);
         }
         if (escalaProgreso_Humedo.x >= EscalaMax)
         {
             Debug.Log("PERDISTE NIÑITO :(");
+            PanelDerrota.SetActive(true);
+            UI_Desactivar.SetActive(false);
+            BotonHumedecer.gameObject.SetActive(false);
+            BotonSecar.gameObject.SetActive(false);
+            BotonRemover.gameObject.SetActive(false);
         }
         if (escalaProgreso_Humedo.x <= EscalaMin)
         {
             Debug.Log("PERDISTE NIÑITO :(");
+            PanelDerrota.SetActive(true);
+            UI_Desactivar.SetActive(false);
+            BotonHumedecer.gameObject.SetActive(false);
+            BotonSecar.gameObject.SetActive(false);
+            BotonRemover.gameObject.SetActive(false);
         }
         if (escalaProgreso_Olor.x >= EscalaMax)
         {
             Debug.Log("PERDISTE NIÑITO :(");
+            PanelDerrota.SetActive(true);
+            UI_Desactivar.SetActive(false);
+            BotonHumedecer.gameObject.SetActive(false);
+            BotonSecar.gameObject.SetActive(false);
+            BotonRemover.gameObject.SetActive(false);
         }
         if (escalaProgreso_Olor.x <= EscalaMin)
         {
             Debug.Log("PERDISTE NIÑITO :(");
+            PanelDerrota.SetActive(true);
+            UI_Desactivar.SetActive(false);
+            BotonHumedecer.gameObject.SetActive(false);
+            BotonSecar.gameObject.SetActive(false);
+            BotonRemover.gameObject.SetActive(false);
         }
 
         if (escalaProgreso_Humedo.x == 0.5f)
@@ -130,12 +173,21 @@ public class CompostController : MonoBehaviour
                 if(escalaProgreso_Seco.x == 0.5f)
                 {
                     Debug.Log("GANASTE NIÑITO, ERES UN CAMPEÓN");
+                    PanelVictoria.SetActive(true);
+                    UI_Desactivar.SetActive(false);
+                    BotonHumedecer.gameObject.SetActive(false);
+                    BotonSecar.gameObject.SetActive(false);
+                    BotonRemover.gameObject.SetActive(false);
                     gano = true;
                     Compostera.GetChild(randomIndex).gameObject.SetActive(false);
                     Compostera.GetChild(3).gameObject.SetActive(true);
                 }
             }           
-        }
+        }        
+    } 
+
+    void CambiarCompostActiva()
+    {
         //0 Humeda - 1 Seca - 2 Olor - 3 Correcta
         if(escalaProgreso_Humedo.x > 0.5)
         {
@@ -161,9 +213,78 @@ public class CompostController : MonoBehaviour
                 Compostera.GetChild(2).gameObject.SetActive(true);
             }
         }
-        
-    }  
+    } 
     
+    void ActualizarColores()
+    {
+        if(escalaProgreso_Seco.x == 0.5f)
+        {
+            BarraSeco.color = Color.green;
+        }
+        if(escalaProgreso_Humedo.x == 0.5f)
+        {
+            BarraHumedo.color = Color.green;
+        }
+        if(escalaProgreso_Olor.x == 0.5f)
+        {
+            BarraOlor.color = Color.green;
+        }
+
+        if(escalaProgreso_Seco.x < 0.5f)
+        {
+            BarraSeco.color = Color.yellow;
+        }
+        if(escalaProgreso_Humedo.x < 0.5f)
+        {
+            BarraHumedo.color = Color.yellow;
+        }
+        if(escalaProgreso_Olor.x < 0.5f)
+        {
+            BarraOlor.color = Color.yellow;
+        }
+
+        if(escalaProgreso_Seco.x < 0.3f)
+        {
+            BarraSeco.color = Color.red;
+        }
+        if(escalaProgreso_Humedo.x < 0.3f)
+        {
+            BarraHumedo.color = Color.red;
+        }
+        if(escalaProgreso_Olor.x < 0.3f)
+        {
+            BarraOlor.color = Color.red;
+        }   
+
+
+        if(escalaProgreso_Seco.x > 0.5f)
+        {
+            BarraSeco.color = Color.yellow;
+        }
+        if(escalaProgreso_Humedo.x > 0.5f)
+        {
+            BarraHumedo.color = Color.yellow;
+        }
+        if(escalaProgreso_Olor.x > 0.5f)
+        {
+            BarraOlor.color = Color.yellow;
+        }       
+
+        if(escalaProgreso_Seco.x > 0.8f)
+        {
+            BarraSeco.color = Color.red;
+        }
+        if(escalaProgreso_Humedo.x > 0.8f)
+        {
+            BarraHumedo.color = Color.red;    
+        }
+        if(escalaProgreso_Olor.x > 0.8f)
+        {
+            BarraOlor.color = Color.red;    
+        }
+
+
+    }
 
 
 }
