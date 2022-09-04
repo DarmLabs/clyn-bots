d4220bb38fe913c8f025be79c0077d7836a3f865
BDeWinne <68663886+BDeWinne@users.noreply.github.com>
2022-09-04 19:18:40 -0300

    refinador progress

---
+++ Assets/Scripts/UI/OnPointerOver.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class OnPointerOver : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public void OnPointerEnter(PointerEventData data){
+
+    }
+    public void OnPointerExit(PointerEventData data){
+        
+    }
+}
