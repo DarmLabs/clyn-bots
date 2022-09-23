392ce0672c376342d2a97aa8158fcd688b0d0e4f
julagui <julioaguilar-ap@outlook.com>
2022-09-23 15:54:08 -0300

    Central timescale1

--- Assets/Scripts/UI/Minigames/MG_UI_Manager.cs
+++ Assets/Scripts/UI/Minigames/MG_UI_Manager.cs
@@ -60,7 +60,7 @@ public class MG_UI_Manager : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         Grilla.vidas = 20;
-        //Time.timeScale = 1f;
+        Time.timeScale = 1f;
         /*switch (Tachos.CantidadGrilla)
         {
             case 24:
